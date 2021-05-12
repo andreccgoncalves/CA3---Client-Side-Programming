bc758d7b24116f998fefeda8d699e629a0e31c1b
andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>
2021-05-12 13:35:25 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,16 +7,17 @@ function validate() {
 
     if (regex.test(staffNumber)) {
         alert('Valid Staff Number');
+        
     }
     else {
         alert('Invalid Staff Number');
+    
     }
+
 
 
 
     document.getElementById('getRandom').addEventListener('click', getRandom);
-
-
 
     function getRandom() {
         fetch('https://randomuser.me/api/?results=5')
@@ -45,9 +46,101 @@ function validate() {
                 });
 
                 document.getElementById('output').innerHTML = output;
-
+               
             })
     }
+
+
+ 
+
+//Starters value
+
+var total_items = 3;
+
+    function CalculateStartersValue() {
+
+        var total = 0;
+
+        for (let i = 1; i <= 3; i++) {
+            itemID = document.getElementById("qnt_" + i);
+            total = total + (parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price")))
+
+        }
+        document.getElementById('StartersTotal').innerHTML = "€" + total.toFixed(2);
+       
+    }
+
+    document.querySelectorAll('[id^="qnt_1"], [id^="qnt_2"], [id^="qnt_3"]').forEach(item => {
+        item.addEventListener('keyup', CalculateStartersValue);
+    });
+
+    //Main value
+
+    var total_items = 3;
+
+    function CalculateMainValue() {
+
+        var total = 0;
+
+        for (let i = 1; i <= 3; i++) {
+            itemID = document.getElementById("qnt_" + i);
+            total = total + (parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price")))
+
+        }
+        document.getElementById('MainTotal').innerHTML = "€" + total.toFixed(2);
+       
+    }
+
+    document.querySelectorAll('[id^="qnt_4"], [id^="qnt_5"], [id^="qnt_6"]').forEach(item => {
+        item.addEventListener('keyup', CalculateMainValue);
+    });
+
+    //Dessert value
+
+var total_items = 3;
+
+function CalculateSDessertValue() {
+
+    var total = 0;
+
+    for (let i = 1; i <= 3; i++) {
+        itemID = document.getElementById("qnt_" + i);
+        total = total + (parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price")))
+
+    }
+    document.getElementById('DesertTotal').innerHTML = "€" + total.toFixed(2);
+   
+}
+
+document.querySelectorAll('[id^="qnt_7"], [id^="qnt_8"], [id^="qnt_9"]').forEach(item => {
+    item.addEventListener('keyup', CalculateSDessertValue);
+});
+
+
+//Drinks value
+
+var total_items = 3;
+
+function CalculateSDrinkValue() {
+
+    var total = 0;
+
+    for (let i = 1; i <= 3; i++) {
+        itemID = document.getElementById("qnt_" + i);
+        total = total + (parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price")))
+
+    }
+    document.getElementById('DrinksTotal').innerHTML = "€" + total.toFixed(2);
+   
+}
+
+document.querySelectorAll('[id^="qnt_10"], [id^="qnt_11"], [id^="qnt_12"]').forEach(item => {
+    item.addEventListener('keyup', CalculateSDrinkValue);
+});
+
+
+// Total calculator
+
     var total_items = 12;
 
     function CalculateItemsValue() {
@@ -59,7 +152,8 @@ function validate() {
             total = total + (parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price")))
 
         }
-        document.getElementById('ItemsTotal').innerHTML = "$" + total;
+        document.getElementById('ItemsTotal').innerHTML = "€" + total.toFixed(2);
+        
     }
 
     document.querySelectorAll('[id^="qnt_"]').forEach(item => {
